a0a3c7b2768f1ea115f892caddac1b8cbe039871
surranc20 <surranc20@mail.wlu.edu>
2024-02-18 11:39:08 -0600

    mining program

--- mine.lua
+++ mine.lua
@@ -5,14 +5,49 @@ function mine_line(line_len)
         line_len -= 1
     end
 
--- fix to work with odds
+function travel_line(line_len)
+    while line_len > 0 do
+        turtle.forward()
+        line_len -= 1
+    end
+
 function dig_square(side_len)
     lines_to_dig = side_len
-    while lines_to_dig > 1 do
-        mine_line(side_len)
-        turtle.turnRight()
-        turtle.dig()
-        -- might need to go one less
+    on_starting_side = true
+
+    while true do
         mine_line(side_len)
         lines_to_dig -= 1
+        on_starting_side = not on_starting_side
+
+        if lines_to_dig == 0 then
+            break
+        end
+
+        if on_starting_side then
+            turtle.turnLeft()
+            turtle.dig()
+            turtle.forward()
+            turtle.turnLeft()
+        else
+            turtle.turnRight()
+            turtle.dig()
+            turtle.forward()
+            turtle.turnRight()
+
+    if not on_starting_side then
+        turtle.turnRight()
+        turtle.turnRight()
+        travel_line(side_len)
     end
+
+    turtle.turnRight()
+    travel_line(side_len)
+    turtle.turnRight()
+end
+
+
+
+
+
+
